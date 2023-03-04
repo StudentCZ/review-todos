a3a619ec87a544c83e90cb64002c2a7342291e9a
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 14:31:50 -0500

    added a post call request in calls

--- client/src/components/calls.js
+++ client/src/components/calls.js
@@ -1,6 +1,18 @@
-export const AddToForm = async (e) => {
-  e.preventDefault();
+import React, { useState } from 'react';
 
+export const AddToForm = async (e) => {
+  const [description, setDescription] = useState('');
+  e.preventDefault();
   try {
-  } catch (error) {}
+    const body = { description };
+    const response = await fetch('http://localhost:5001/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
