066d39f3fa501e489aeba3eb3807ed3bcd9cc79d
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 13:59:28 -0500

    added some css to input form

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -5,9 +5,9 @@ const InputTodo = () => {
   return (
     <>
       <h1 className={styles.heading}>My Todo List</h1>
-      <form>
-        <input type='text' />
-        <button></button>
+      <form className={styles.form1}>
+        <input type='text' className={styles.input1} />
+        <button className={styles.button1}>ADD</button>
       </form>
     </>
   );
