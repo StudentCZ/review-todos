75448894f078fb03c9d9279716c4c1e1b5b7ae1a
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 18:57:04 -0500

    created a getTodos function and useEffect hook

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import main from './main.module.css';
 import styles from './ListTodos.module.css';
 
 const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/todos');
+      const data = await response.json();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <>
       <table className={styles.table}>
@@ -11,11 +26,11 @@ const ListTodos = () => {
           <th>Edit</th>
           <th>Delete</th>
         </tr>
-        <tr>
+        {/* <tr>
           <td>Alfreds Futterkiste</td>
           <td>Maria Anders</td>
           <td>Germany</td>
-        </tr>
+        </tr> */}
       </table>
     </>
   );
