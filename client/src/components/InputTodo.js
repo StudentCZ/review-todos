4e631d0fe2a8850f00552453b40859a94d7db249
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 13:14:18 -0500

    center the heading InputTodo component

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './InputTodo.module.css';
 
 const InputTodo = () => {
   return (
     <>
-      <h1 className='heading'> Todo List</h1>
+      <h1 className={styles.heading}> Todo List</h1>
     </>
   );
 };
