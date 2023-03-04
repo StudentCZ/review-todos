f28155f460b17674fcca8b0768437b1da1f4eec5
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 11:40:18 -0500

    put request works to update a todo

--- server/routes.js
+++ server/routes.js
@@ -36,4 +36,18 @@ router.get('/todos/:id', async (req, res) => {
   }
 });
 
+router.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      `UPDATE todo SET description = $1 WHERE todo_id = $2`,
+      [description, id]
+    );
+    res.json('todo was updated');
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 module.exports = router;
