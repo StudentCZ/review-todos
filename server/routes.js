72a0e426dc69a4002a10961dff80cad20e5f5e5d
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 11:21:58 -0500

    have a working post request and tested on postman

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,17 @@ const pool = require('./db');
 
 router.use(express.json());
 
-router.post('/todos', async (req, res) => {});
+router.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      `INSERT INTO todo (description) VALUES($1) RETURNING *`,
+      [description]
+    );
+    res.json(newTodo.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 module.exports = router;
