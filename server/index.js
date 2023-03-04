0bd2778c9fc22eece432881493684d02046c20cc
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 11:15:18 -0500

    separated routes into it own file and create boilerplate post request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db');
+const routes = require('./routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/', routes);
 
 app.listen(5001, () => {
   console.log('server has started on port 5001');
--- server/routes.js
+++ server/routes.js
@@ -3,3 +3,7 @@ const router = express.Router();
 const pool = require('./db');
 
 router.use(express.json());
+
+router.post('/todos', async (req, res) => {});
+
+module.exports = router;
