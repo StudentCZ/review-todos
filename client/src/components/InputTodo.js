0a804dc9974476f330b840e4dc952a02b9801729
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 13:59:15 -0500

    added a form to add todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -4,7 +4,11 @@ import styles from './InputTodo.module.css';
 const InputTodo = () => {
   return (
     <>
-      <h1 className={styles.heading}> Todo List</h1>
+      <h1 className={styles.heading}>My Todo List</h1>
+      <form>
+        <input type='text' />
+        <button></button>
+      </form>
     </>
   );
 };
