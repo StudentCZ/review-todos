ce56b7ed1e974bfc6d86e2a9d2ee69f02a55d214
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 22:53:00 -0500

    refactored the code a bit

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,32 @@ import { AddToForm } from './components/api';
 import InputTodo from './components/InputTodo';
 import ListTodos from './components/ListTodos';
 
-function App() {
+const App = () => {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const handleAddTodo = async (description) => {
+    setLoading(true);
+
     try {
-      setLoading(true); // set loading state to true before sending POST request
-      const response = await AddToForm(
-        {
-          description: description,
-        },
-        setTodos // pass setTodos function to update state after successful POST
-      );
-      const newTodo = response.data;
-      setTodos((prevTodos) => [...prevTodos, newTodo]);
-      setLoading(false); // set loading state to false after receiving response
+      const response = await AddToForm({ description }, setTodos);
+      setTodos((prevTodos) => [...prevTodos, response.data]);
     } catch (error) {
-      console.log(error);
-      setLoading(false); // set loading state to false if an error occurs
+      console.error(error.message);
     }
+    setLoading(false);
   };
 
   return (
-    <>
-      <div className='container'>
-        <InputTodo handleAddTodo={handleAddTodo} />
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ListTodos todos={todos} setTodos={setTodos} />
-        )}
-      </div>
-    </>
+    <div className='container'>
+      <InputTodo handleAddTodo={handleAddTodo} />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ListTodos todos={todos} setTodos={setTodos} />
+      )}
+    </div>
   );
-}
+};
 
 export default App;
