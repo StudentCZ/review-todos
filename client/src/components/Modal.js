b445f1c95c0c50e6089d0d32adcc4e8f58f22ffd
Chao Zhou <Czhou0k@gmail.com>
2023-03-05 11:59:02 -0500

    added code to the Modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './Modal.module.css';
 
-const Modal = () => {
-  return <></>;
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+  return (
+    <>
+      <div className={styles.modal}>
+        <div className={styles.modal_overlay} onClick={onClose} />
+        <div className={styles.modal_content}>
+          <button className={styles.modal_close} onClick={onClose}>
+            &times;
+          </button>
+        </div>
+        {children}
+      </div>
+    </>
+  );
 };
 
 export default Modal;
