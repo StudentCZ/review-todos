3488bbd06c3903b9af0b8152c12efa9bad14e22e
Chao Zhou <Czhou0k@gmail.com>
2023-03-04 23:03:45 -0500

    refactor inputTodo.js

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -13,11 +13,15 @@ const InputTodo = ({ handleAddTodo }) => {
     <>
       <h1 className={styles.heading}>My Todo List</h1>
       <form className={styles.form1} onSubmit={handleSubmit}>
+        <label htmlFor='description' className={styles.label}>
+          Description
+        </label>
         <input
           type='text'
           className={styles.input1}
           value={description}
           onChange={(e) => setDescription(e.target.value)}
+          placeholder='Enter a new todo'
         />
         <button className={styles.button1}>ADD</button>
       </form>
